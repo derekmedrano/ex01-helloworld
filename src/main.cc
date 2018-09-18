a67520b229e2bad21986185c198fd6bc3e418c17
VC Science Labs <vscilab@vcccd.edu>
2018-09-18 08:31:25 -0700

    Turning In

---
+++ src/main.cc
@@ -0,0 +1,19 @@
+/**
+* CS V13 Object-Oriented Programming
+* CRN: 70226
+* Assignment: EX01-HelloWorld
+*
+* Statement of code ownership: I hereby state that I have written all of this
+* code and I have not copied this code from any other person or source.
+*
+* @author Travis Danford Chamness
+*/
+
+#include <iostream>
+#include <csv13>
+using std::cout;
+using std::endl;
+int main(int argc, char *argv[]) {
+	cout << "Hello, World!" << endl;
+	return EXIT_SUCCESS;
+}
